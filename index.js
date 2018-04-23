de05b4eeaea0a69cf8644189c56350ffdce86008
Kamil Stangret <kamilstangret@wp.pl>
2018-04-23 21:13:48 +0200

    Modify formatDate function

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const formatDate = (timeInSeconds) => {
-  return `${timeInSeconds}s`;
-}
+	let seconds = Math.floor(timeInSeconds),
+			minutes = Math.floor(seconds / 60),
+			hours = Math.floor(minutes / 60);
+	
+	if (timeInSeconds == null) {
+		return `0s`;
+		
+	} else if (seconds > 3600) {
+		seconds = seconds % 60; 
+		minutes = minutes % 60;
+
+		if (seconds == 0 && minutes == 0) {
+			return `${hours}h`;
+
+		} else if (minutes == 0) {
+			return `${hours}h ${seconds}s`;
+
+		} else if (seconds == 0) {
+			return `${hours}h ${minutes}m`;
+		}
+
+		return `${hours}h ${minutes}m ${seconds}s`;
+
+	} else if (seconds > 60) {
+		seconds = seconds % 60; 
+
+		if (seconds == 0) {
+			return `${minutes}m`;
+		}
+
+		return `${minutes}m ${seconds}s`;
+	}
+
+	return `${timeInSeconds}s`;
+};
 
 module.exports = formatDate;
